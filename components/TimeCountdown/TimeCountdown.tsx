19da0d379a5816321963b2fcbcabfa9e4702abe0
lyy16 <lvyueying@8lab.com>
2024-01-19 19:32:19 +0800

    bug: S Chinese

--- components/TimeCountdown/TimeCountdown.tsx
+++ components/TimeCountdown/TimeCountdown.tsx
@@ -1,52 +1,55 @@
-import React from "react"
-import Countdown, { CountdownRenderProps } from "react-countdown"
-import { useMemo } from "react"
-import styled from "@emotion/styled"
+import React from "react";
+import Countdown, { CountdownRenderProps } from "react-countdown";
+import { useMemo } from "react";
+import styled from "@emotion/styled";
 
 interface ProgressCountdownProps {
-  deadline: Date,
-  onComplete:any,
+  deadline: Date;
+  onComplete: any;
 }
 
 const TimeCountdown: React.FC<ProgressCountdownProps> = ({
   deadline,
-  onComplete
+  onComplete,
 }) => {
   const deadlineVal = useMemo(() => {
-    return deadline.getTime()
-  }, [deadline])
+    return deadline.getTime();
+  }, [deadline]);
   const countdownRenderer = (countdownProps: CountdownRenderProps) => {
-    const { days, hours, minutes, seconds } = countdownProps
-    const d = String(days)
-    const h = String(hours)
-    const m = String(minutes)
-    const s = String(seconds)
+    const { days, hours, minutes, seconds } = countdownProps;
+    const d = String(days);
+    const h = String(hours);
+    const m = String(minutes);
+    const s = String(seconds);
     if (Number(d) > 0) {
       return (
         <StyledCountdown>
-          {d.padStart(2, "0")}D {h.padStart(2, "0")}H {m.padStart(2, "0")}{" "}
-          分
+          {d.padStart(2, "0")}D {h.padStart(2, "0")}H {m.padStart(2, "0")}S
         </StyledCountdown>
-      )
+      );
     }
     return (
       <StyledCountdown>
         {h.padStart(2, "0")}H {m.padStart(2, "0")}M {s.padStart(2, "0")}S
       </StyledCountdown>
-    )
-  }
+    );
+  };
   return (
     <StyledCardContentInner>
-      <Countdown onComplete={onComplete} date={deadlineVal} renderer={countdownRenderer} />
+      <Countdown
+        onComplete={onComplete}
+        date={deadlineVal}
+        renderer={countdownRenderer}
+      />
     </StyledCardContentInner>
-  )
-}
+  );
+};
 const StyledCountdown = styled.p`
   /* line-height: 1.8;
   margin-top: 10px;
   font-size: 14px;
   color: rgba(255, 255, 255, 0.7); */
-`
+`;
 
 const StyledCardContentInner = styled.div`
   // height: 100%;
@@ -54,6 +57,6 @@ const StyledCardContentInner = styled.div`
   /* align-items: center; */
   justify-content: center;
   flex-direction: column;
-`
+`;
 
-export default TimeCountdown
+export default TimeCountdown;
