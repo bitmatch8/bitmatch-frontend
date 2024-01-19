2edbfdd1c1acdbb585e30bafa18e86f8806bcb48
lyy16 <lvyueying@8lab.com>
2024-01-19 11:09:30 +0800

    style: footer github hide

--- components/Page/Footer/LinkIcons.tsx
+++ components/Page/Footer/LinkIcons.tsx
@@ -1,41 +1,44 @@
-import GithubIcon from "@/components/Svg/GithubIcon"
-import XIcon from "@/components/Svg/XIcon"
-import TelegramIcon from "@/components/Svg/TelegramIcon"
-import styled from "@emotion/styled"
-import Link from "next/link"
-import GitbookIcon from "@/components/Svg/GitbookIcon"
+import GithubIcon from "@/components/Svg/GithubIcon";
+import XIcon from "@/components/Svg/XIcon";
+import TelegramIcon from "@/components/Svg/TelegramIcon";
+import styled from "@emotion/styled";
+import Link from "next/link";
+import GitbookIcon from "@/components/Svg/GitbookIcon";
 
-const LinkIcon: React.FC<{ to: string; icon: "Github" | "X" | "Telegram" | "GitbookIcon"}> = ({icon, to = "#"}) => {
+const LinkIcon: React.FC<{
+  to: string;
+  icon: "Github" | "X" | "Telegram" | "GitbookIcon";
+}> = ({ icon, to = "#" }) => {
   const SvgIcons = {
-    "Github": GithubIcon,
-    "X": XIcon,
-    "Telegram": TelegramIcon,
-    "GitbookIcon": GitbookIcon,
-  }
-  const Svg = SvgIcons[icon]
+    Github: GithubIcon,
+    X: XIcon,
+    Telegram: TelegramIcon,
+    GitbookIcon: GitbookIcon,
+  };
+  const Svg = SvgIcons[icon];
   return (
     <FooterLinkItemBox target="_blank" href={to}>
       <Svg fill="#c2c5c8" width={48} />
     </FooterLinkItemBox>
-  )
-}
+  );
+};
 
 export default () => {
   return (
     <FooterLinksBox>
       <LinkIcon icon="X" to="https://twitter.com/deindex_BTC" />
-      <LinkIcon icon="Github" to="#" />
+      {/* <LinkIcon icon="Github" to="#" /> */}
       <LinkIcon icon="Telegram" to="https://t.me/deindexbtc" />
       <LinkIcon icon="GitbookIcon" to="https://public-9.gitbook.io/deindex/" />
     </FooterLinksBox>
-  )
-}
+  );
+};
 
 const FooterLinksBox = styled.div`
   display: flex;
   align-items: center;
   gap: 50px;
-`
+`;
 const FooterLinkItemBox = styled(Link)`
   display: block;
   width: 48px;
@@ -46,4 +49,4 @@ const FooterLinkItemBox = styled(Link)`
       fill: #ffffff;
     }
   }
-`
+`;
