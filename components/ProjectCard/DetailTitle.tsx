10897993af88e3524e879034b9c6c0d084c2489a
lyy16 <lvyueying@8lab.com>
2024-01-22 13:42:06 +0800

    style: page title

--- components/ProjectCard/DetailTitle.tsx
+++ components/ProjectCard/DetailTitle.tsx
@@ -1,32 +1,37 @@
 /* Components */
 
-import Button from "@/components/Button"
-import styled from "@emotion/styled"
-import ArrowLeftIcon from "@/components/Svg/ArrowLeftIcon"
-import { useRouter } from "next/navigation"
-import React from "react"
-import ValueSkeleton from "@/components/ValueSkeleton"
+import Button from "@/components/Button";
+import styled from "@emotion/styled";
+import ArrowLeftIcon from "@/components/Svg/ArrowLeftIcon";
+import { useRouter } from "next/navigation";
+import React from "react";
+import ValueSkeleton from "@/components/ValueSkeleton";
 
-const DetailTitle:React.FC<{title:string}>=({title})=>{
-	const { back } = useRouter()
-	return <HeadContainerBox>
-        <HeadBackButtonBox onClick={back}>
-          {title === null ? <ValueSkeleton width={800} height={100}/> : <>
-          <ArrowLeftIcon width={100} fill="#DBDBDB" />
-          <span>{title}</span>
-          </>} 
-        </HeadBackButtonBox>
-      </HeadContainerBox>
-}
-export default DetailTitle
+const DetailTitle: React.FC<{ title: string }> = ({ title }) => {
+  const { back } = useRouter();
+  return (
+    <HeadContainerBox>
+      <HeadBackButtonBox onClick={back}>
+        {title === null ? (
+          <ValueSkeleton width={800} height={100} />
+        ) : (
+          <>
+            <ArrowLeftIcon width={63} fill="#DBDBDB" />
+            <span>{title}</span>
+          </>
+        )}
+      </HeadBackButtonBox>
+    </HeadContainerBox>
+  );
+};
+export default DetailTitle;
 const HeadBackButtonBox = styled(Button)`
   background-color: transparent;
   width: auto;
-  font-size: 60px;
-  font-weight: 600;
-  min-height: 100px;
+  font-size: 40px;
   color: #dbdbdb;
-  line-height: 100px;
+  line-height: 63px;
+  font-family: Montserrat;
   &:hover {
     color: #f8931a;
     svg {
@@ -35,7 +40,7 @@ const HeadBackButtonBox = styled(Button)`
     background: transparent;
   }
   gap: 36px;
-`
+`;
 const HeadContainerBox = styled.div`
-  margin: 145px auto 0px;
-`
+  margin: 100px auto 0px;
+`;
--- pages/genesis-nft/GenesisNFT.tsx
+++ pages/genesis-nft/GenesisNFT.tsx
@@ -1,12 +1,12 @@
 /* Components */
 
-import Page from "@/components/Page"
-import DetailTitle from "@/components/ProjectCard/DetailTitle"
-import { Spaced } from "@/components/Spaced"
-import ProjectCard from "@/components/ProjectCard"
-import ProjectTabs from "@/components/ProjectTabs"
-import useDetail from "@/hook/useDetail"
-import OrderHistory from "@/components/OrderHistory"
+import Page from "@/components/Page";
+import DetailTitle from "@/components/ProjectCard/DetailTitle";
+import { Spaced } from "@/components/Spaced";
+import ProjectCard from "@/components/ProjectCard";
+import ProjectTabs from "@/components/ProjectTabs";
+import useDetail from "@/hook/useDetail";
+import OrderHistory from "@/components/OrderHistory";
 
 export default function GenesisNFT() {
   const {
@@ -19,11 +19,11 @@ export default function GenesisNFT() {
     publicInfo,
     readWhtie,
     readPublic,
-  } = useDetail(1)
+  } = useDetail(1);
   return (
     <Page>
       <DetailTitle title={detail === null ? null : detail?.projectname} />
-      <Spaced size="80" />
+      <Spaced size="60" />
       <ProjectCard detail={detail} buyType={buyType} />
       <ProjectTabs
         tabId={tabId}
@@ -35,7 +35,7 @@ export default function GenesisNFT() {
         publicRead={readPublic}
       />
       <Spaced size="150" />
-      <OrderHistory address={address} pid={1}/>
+      <OrderHistory address={address} pid={1} />
     </Page>
-  )
+  );
 }
