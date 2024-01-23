2e3c9643ec3e6fd72a48d8a20f3103e8b9312949
light10917 <light10917@gmail.com>
2024-01-23 10:49:29 +0800

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const prod = process.env.NODE_ENV === 'production';
 
+console.log({prod})
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
@@ -14,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `http://101.251.211.205:8066/:path*`
+        destination: prod? `http://192.168.1.75:58066/:path*` : `http://101.251.211.205:8066/:path*`
       },
     ]
   }
