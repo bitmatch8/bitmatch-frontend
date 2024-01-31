d7bb1ed1840607f1cb141449c2feea5332fefdcf
light10917 <light10917@gmail.com>
2024-01-31 14:49:44 +0800

    edit package.json

--- lib/redux/slices/detailSlice/thunks.ts
+++ lib/redux/slices/detailSlice/thunks.ts
@@ -1,6 +1,4 @@
 /* Instruments */
-import useWallter from "@/hook/useWallter"
-import { selectWallter, type ReduxThunkAction } from "@/lib/redux"
 import { createAppAsyncThunk } from "@/lib/redux/createAppAsyncThunk"
 
 // The function below is called a thunk and allows us to perform async logic. It
@@ -11,25 +9,5 @@ import { createAppAsyncThunk } from "@/lib/redux/createAppAsyncThunk"
 export const fetchDetailThunk = createAppAsyncThunk(
   "wallter/connectUnisat",
   async (_,{getState}) => {
-    const {wallter} = useWallter()
-    const {network} = selectWallter(getState())
-    if (network === process.env.NEXT_PUBLIC_NETWORK) {
-      const [address] = await wallter.requestAccounts()
-      return {address,network}
-    } else {
-      const network = await wallter.switchNetwork(process.env.NEXT_PUBLIC_NETWORK)
-      if(network === process.env.NEXT_PUBLIC_NETWORK){
-        const [address] = await wallter.requestAccounts()
-        return {address,network}
-      }
-    }
-    
-    return {address:'',network}
-
-    // handleAccountsChanged(result);
-    // const [address] = await unisat.getAccounts()
-    // console.log({address},await unisat.getAccounts().then(console.log))
-    // return {address}
-    // The value we return becomes the `fulfilled` action payload
   }
 )
