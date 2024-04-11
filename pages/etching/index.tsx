2c0885a3eacdb94d7d73b29eb9e547e0de51ef71
qiexingyu1 <qiexingyu@8lab.cn>
2024-04-11 14:57:43 +0800

    新建页面

--- pages/etching/index.tsx
+++ pages/etching/index.tsx
@@ -1,9 +1,19 @@
 import Page from "@/components/Page"
+import History from "./History"
 
 export default function IndexPage() {
     return (
         <Page>
-            sdf
+            <p style={{ color: '#FFF', textAlign: 'center' }}>Runes</p>
+            <div style={{ color: '#FFF', textAlign: 'center' }}>
+                <span>Etching</span>
+                <span style={{marginLeft: '20px'}}>Mint</span>
+                <span style={{marginLeft: '20px'}}>Transfer</span>
+            </div>
+            <div style={{ height: '300px', border: '1px solid red' }}>
+
+            </div>
+            <History></History>
         </Page>
     )
 }
---
+++ pages/search/index.tsx
@@ -0,0 +1,9 @@
+import Page from "@/components/Page"
+
+export default function SearchPage() {
+    return (
+        <Page>
+            <p style={{ color: '#FFF', textAlign: 'center' }}>search</p>
+        </Page>
+    )
+}
