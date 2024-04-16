813dd8982bfea913940f35b900a28def0a11f09a
qiexingyu1 <qiexingyu@8lab.cn>
2024-04-16 17:25:22 +0800

    隐藏history引入

--- pages/etching/index.tsx
+++ pages/etching/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Page from "@/components/Page";
-import History from "./History";
+// import History from "./History";
 import Etching1 from "./Etching1";
 import Flow2 from "./Flow2";
 import Flow3 from "./Flow3";
